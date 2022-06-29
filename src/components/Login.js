168558a1e30ef7dcac741da22623c078ed0fa16d
aelbanyard <64694151+aelbanyard@users.noreply.github.com>
2022-06-29 23:40:59 +0200

    fix error

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from './Home'
 import Open from './Open'
 import Mine from './Mine'
 import Person from './Person'
