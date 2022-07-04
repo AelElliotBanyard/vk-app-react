b5a8386901df6d0e7d966722b73065ae9237079c
aelbanyard <64694151+aelbanyard@users.noreply.github.com>
2022-07-04 09:45:17 +0200

    Fixed Warnings

--- src/function/Login.js
+++ src/function/Login.js
@@ -26,6 +26,7 @@ export default function Login() {
 
     const [username , setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [user, setUser] = useState(null);
     const [validation, setValidation] = useState(false);
     const usernameRef = useRef(null);
     const passwordRef = useRef(null);
@@ -34,16 +35,17 @@ export default function Login() {
         setUsername(usernameRef.current.value);
         setPassword(passwordRef.current.value);
         const email = username + "@vkaw.ch";
-        const user = null;
         signInWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
                 setValidation(true)
-                user = userCredential.user;
+                setUser(userCredential.user);
             })
             .catch((error) => {
                 setValidation(false);
                 const errorCode = error.code;
+                console.log("Error: "+errorCode);
                 const errorMessage = error.message;
+                console.log("Because: "+ errorMessage);
             });
     }
 
@@ -53,8 +55,8 @@ export default function Login() {
                 <Routes>
                     <Route path='/vk-app-react/' element={<Home/>}/>
                     <Route path='/vk-app-react/open' element={<Open/>}></Route>
-                    <Route path='/vk-app-react/mine' element={<Mine username={username}/>}></Route>
-                    <Route path='/vk-app-react/person' element={<Person username={username}/>}></Route>
+                    <Route path='/vk-app-react/mine' element={<Mine username={username} user={user}/>}></Route>
+                    <Route path='/vk-app-react/person' element={<Person username={username} user={user}/>}></Route>
                     <Route path='/vk-app-react/*' element={<Navigate to="/vk-app-react/" replace />}/>
                 </Routes>
             </BrowserRouter>
